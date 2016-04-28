98b824120d948ec02685aa8c64fe55d2e87785a8
TheSAguy <vvwmisc@hotmail.com>
2016-04-28 10:48:50 -0700

    Reach Removed.

--- Bio_Farm/data-updates.lua
+++ Bio_Farm/data-updates.lua
@@ -3,14 +3,16 @@ NEConfig = {}
 require "libs.detectmod" --Detect sif NE Buildings exist
 data.raw.item["raw-wood"].stack_size = 200
 
+
 --- Got tierd of reaching limits...
+--[[
 if data.raw.player.player.build_distance < 24 then
 	data.raw.player.player.build_distance = 24
 	data.raw.player.player.reach_distance = 20
 	data.raw.player.player.reach_resource_distance = 20
 	data.raw.player.player.drop_item_distance = 20
 end 
-
+]] 
 
 ------- Adds a Mk3 recipe for wood if you're playing with Natural Evolution Buildings
 if NEConfig.mod.NEBuildings then
