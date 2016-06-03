3c8f69b02baa7cac9ed8aae63f96c2904858e8fc
TheSAguy <vvwmisc@hotmail.com>
2016-06-03 10:45:48 -0700

    Small Tweak

--- Bio_Farm/data.lua
+++ Bio_Farm/data.lua
@@ -33,24 +33,3 @@ require("prototypes.Bio_Garden.recipe")
 -- Tech
 require("prototypes.technology")
 
-
-------- DyTech Support
-if BI_Config.mod.DyTechCore then
-
-require("prototypes.Bio_Farm.dytech_recipe")
-	
-	bobmods.lib.add_technology_recipe ("bf_bio_farming", "resin")
-	bobmods.lib.add_technology_recipe ("bf-fertilizer", "resin_Mk2")
-	bobmods.lib.add_technology_recipe ("bf_bio_farming", "sulfur-wood")
-	bobmods.lib.add_technology_recipe ("bf-fertilizer", "sulfur-wood_Mk2")
-
-	
-	--- If you're using NE Buildings, add an advanced recipe
-	if BI_Config.mod.NEBuildings then
-		bobmods.lib.add_technology_recipe ("bf-advanced-biotechnology", "resin_Mk3")
-		bobmods.lib.add_technology_recipe ("bf-advanced-biotechnology", "sulfur-wood_Mk3")
-		table.insert(data.raw.recipe["resin_Mk3"].ingredients,{type="item", name="bf-adv-fertilizer", amount=5})
-		table.insert(data.raw.recipe["sulfur-wood_Mk3"].ingredients,{type="item", name="bf-adv-fertilizer", amount=5})
-	end
-
-end
